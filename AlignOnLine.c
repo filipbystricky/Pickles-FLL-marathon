f03bad2ffddbedb42846140e6d24cb5ec1b32305
Geoffrey Ramseyer <ramseyer@uchicago.edu>
2015-12-22 04:57:58 -0500

    auto attempt

--- AlignOnLine.c
+++ AlignOnLine.c
@@ -1,5 +1,40 @@
 #include "PickleFLLLightDriver.c"
 
+void alignOnLineColor(int power) {
+
+motor[left]=power;
+	motor[right]=power;
+	int leftCount = 0;
+	int rightCount = 0;
+	bool leftFound = false;
+	bool rightFound = false;
+	while(1)
+	{
+		//nxtDisplayCenteredTextLine(0, "%d %d", CSvalNorm(leftLight), CSvalRaw(leftLight));
+		//nxtDisplayCenteredTextLine(1, "%d %d", CSvalNorm(rightLight), CSvalRaw(rightLight));
+
+		if (SensorValue(leftLight) == 1)
+		{
+			motor[left] = 0;
+			leftFound = true;
+			motor[right] = (rightFound?0:power/2);
+		}
+		if (sensorValue(rightLight) == 1)
+		{
+			motor[right] = 0;
+			rightFound = true;
+			motor[left]= (leftFound?0:power/2);
+		}
+		if (rightFound && leftFound) {
+		//			nxtDisplayCenteredTextLine(0, "%d %d", CSvalNorm(leftLight), CSvalRaw(leftLight));
+		//nxtDisplayCenteredTextLine(1, "%d %d", CSvalNorm(rightLight), CSvalRaw(rightLight));
+
+			//while(1);
+			return;
+		}
+
+	}
+}
 #define blackThreshold 10
 
 void alignOnLine(int power) {
